ddb79ae8ea9fabd473cdffcdeb4bbf35b4d6ea7e
CJ <47445339+xkaze09@users.noreply.github.com>
2021-12-02 13:35:30 +0800

    Initial commit

---
+++ 2DShooting.py
@@ -0,0 +1,69 @@
+from ursina import *
+from ursina.prefabs.platformer_controller_2d import PlatformerController2d
+
+app = Ursina()
+window.fullscreen = True
+player = Entity(model='quad', texture='assets\player',
+                collider='box', y=5, scale=2)
+bg = Entity(model='quad', texture='assets\BG', scale=36, z=1)
+target = Entity(model='cube', texture='assets\\target1',
+                collider='box', scale=2, x=20, y=-10)
+
+
+targets = []
+
+
+def newTarget():
+    new = duplicate(target, y=-5+(5124*time.dt) % 15)
+    targets.append(new)
+    invoke(newTarget, delay=1)
+
+
+newTarget()
+camera.orthographic = True
+camera.fov = 20
+
+
+def update():
+    global score, text
+    player.y += held_keys['w'] * 6 * time.dt
+    player.y -= held_keys['s'] * 6 * time.dt
+    player.x += held_keys['d'] * 6 * time.dt
+    player.x -= held_keys['a'] * 6 * time.dt
+    a = held_keys['w'] * -20
+    b = held_keys['s'] * 20
+    if a != 0:
+        player.rotation_z = a
+    else:
+        player.rotation_z = b
+    for target in targets:
+        target.x -= 4*time.dt
+        touch = target.intersects()
+        if touch.hit:
+            targets.remove(target)
+            destroy(target)
+            destroy(touch.entity)
+            score += 1
+            text.y = 10
+            text = Text(text=f"Score: {score}", position=(-.65, .4),
+                        origin=(0, 0), scale=2, color=color.yellow, background=True)
+    t = player.intersects()
+    if t.hit and t.entity.scale == 2:
+        quit()
+
+
+def input(key):
+    if key == 'space':
+        e = Entity(y=player.y+0.6, x=player.x+1.3, model='cube', scale=1,
+                   texture='assets\Bullet', collider='cube')
+        e.animate_x(30, duration=2, curve=curve.linear)
+        invoke(destroy, e, delay=2)
+
+
+score = 0
+text = Text(text='')
+text = Text(text=f"Score: {score}", position=(-.65, .4),
+            origin=(0, 0), scale=2, color=color.yellow, background=True)
+
+
+app.run()
